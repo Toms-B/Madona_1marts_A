f8fccbd97b3d2418e38ff3b4bde9675616bca5d4
Toms-B <toms.bergis06@gmail.com>
2024-03-01 15:25:40 +0200

    3.uzd un 1.uzd

--- kd2.py
+++ kd2.py
@@ -1,5 +1,8 @@
 import PySimpleGUI as sg
 
+import json 
+import csv
+
 def read_file(filename):
     try: 
         with open (filename, 'r', encoding='utf8') as file:
@@ -9,6 +12,19 @@ def read_file(filename):
     except Exception as e:
         return f"error:{e}"
     
+def read_json_file(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        return data
+    
+def read_csv_file(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+        return data
+    
+
+
 def main():
     layout=[
     [sg.Text('Ievadiet txt datnes nosaukumu:')],
